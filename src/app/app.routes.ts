bd5736aeca8ab2aaac8a35cfad075eb44b163007
dvd2001 <bedavid2001@gmail.com>
2026-07-06 17:14:41 +0200

    Adding game page to the components and to the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { StartPage } from './components/start-page/start-page';
+import { GamePage } from './components/game-page/game-page';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'start', pathMatch: 'full' },
     { path: 'start', component: StartPage },
+    { path: 'game', component: GamePage },
 ];
