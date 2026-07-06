bfb6aaefdc839b17177ce2698859e0691b776ac2
dvd2001 <bedavid2001@gmail.com>
2026-07-06 19:32:41 +0200

    Adding navbar and functioning back btn to game page

--- src/app/components/game-page/game-page.ts
+++ src/app/components/game-page/game-page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../../services/theme-service/theme-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-page',
@@ -8,5 +9,10 @@ import { ThemeService } from '../../services/theme-service/theme-service';
   styleUrl: './game-page.css',
 })
 export class GamePage {
-  constructor(themeService: ThemeService) { }
+  constructor(private themeService: ThemeService, private router: Router) { }
+  onBack(): void {
+    window.sessionStorage.removeItem('startField');
+    window.sessionStorage.removeItem('maxOnline');
+    this.router.navigate(['']);
+  }
 }
