2fb229ccaa05e810db0bda638b29a2f01196a660
dvd2001 <bedavid2001@gmail.com>
2026-07-10 22:48:53 +0200

    setting up the html skeleton of game page

--- src/app/components/game-page/game-page.ts
+++ src/app/components/game-page/game-page.ts
@@ -3,10 +3,11 @@ import { ThemeService } from '../../services/theme-service/theme-service';
 import { DataReadingService } from '../../services/data-reading-service/data-reading-service';
 import { Router } from '@angular/router';
 import { Field } from '../../models/Field';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-game-page',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './game-page.html',
   styleUrl: './game-page.css',
 })
@@ -14,6 +15,13 @@ export class GamePage implements OnInit {
   private fields: Field[] = [];
   private startField: number = 0;
   private maxOnline: number = 0;
+  public showLevel1 = false;
+  public showLevel2 = false;
+  public showLevel3 = false;
+  public showLevel4 = false;
+  public showLevel5 = false;
+  public showFlat = false;
+  public showPansion = false;
   constructor(private themeService: ThemeService, private router: Router, private reader: DataReadingService) {
     if (typeof window !== 'undefined') {
       const start = window.sessionStorage.getItem('startField');
