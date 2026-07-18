cd91fe80288a0b7638a89eb7d116b755de519776
dvd2001 <bedavid2001@gmail.com>
2026-07-18 22:34:19 +0200

    Adding dynamic data loading on init

--- src/app/components/game-page/game-page.ts
+++ src/app/components/game-page/game-page.ts
@@ -4,7 +4,6 @@ import { DataReadingService } from '../../services/data-reading-service/data-rea
 import { Router } from '@angular/router';
 import { Field } from '../../models/Field';
 import { NgIf } from '@angular/common';
-import { Console } from 'node:console';
 
 @Component({
   selector: 'app-game-page',
@@ -38,6 +37,27 @@ export class GamePage implements OnInit {
   }
   ngOnInit(): void {
     this.fields = this.reader.readingFields();
+    if (typeof document !== 'undefined') {
+      const goldPrice = document.querySelector('#goldPrice') as HTMLElement;
+      const goldAmount = document.querySelector('#goldAmount') as HTMLElement;
+      const minePrice = document.querySelector('#minePrice') as HTMLElement;
+      const mineAmount = document.querySelector('#mineAmount') as HTMLElement;
+      const chocolatePrice = document.querySelector('#chocolatePrice') as HTMLElement;
+      const chocolateAmount = document.querySelector('#chocolateAmount') as HTMLElement;
+      const flatPrice = document.querySelector('#flatPrice') as HTMLElement;
+      const pansionPrice = document.querySelector('#pansionPrice') as HTMLElement;
+      const onlinePrice = document.querySelector('#onlinePrice') as HTMLElement;
+      const start = this.fields[this.startField];
+      goldPrice.innerText = `$${(start.gold * 1000).toLocaleString('hu-HU')}`;
+      goldAmount.innerText = '0 db';
+      minePrice.innerText = `$${(start.mine * 1000).toLocaleString('hu-HU')}`;
+      mineAmount.innerText = '0 db';
+      chocolatePrice.innerText = `$${(start.chocolate * 1000).toLocaleString('hu-HU')}`;
+      chocolateAmount.innerText = '0 db';
+      flatPrice.innerText = `$${(start.flatBuy * 1000).toLocaleString('hu-HU')}`;
+      pansionPrice.innerText = `$${(start.pansionBuy * 1000).toLocaleString('hu-HU')}`;
+      onlinePrice.innerText = `$${(start.onlineBuy * 1000).toLocaleString('hu-HU')}`;
+    }
   }
   onBack(): void {
     this.router.navigate(['']);
