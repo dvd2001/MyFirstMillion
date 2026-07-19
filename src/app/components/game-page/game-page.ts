259ad042c0dc9629666fc1123fc4af059fe0a3f4
dvd2001 <bedavid2001@gmail.com>
2026-07-20 00:34:45 +0200

    putting up function btns and creating empty click functions

--- src/app/components/game-page/game-page.ts
+++ src/app/components/game-page/game-page.ts
@@ -22,6 +22,7 @@ export class GamePage implements OnInit {
   public showLevel5 = false;
   public showFlat = false;
   public showPansion = false;
+  public mobile = false;
   constructor(public themeService: ThemeService, private router: Router, private reader: DataReadingService) {
     if (typeof window !== 'undefined') {
       const start = window.sessionStorage.getItem('startField');
@@ -36,6 +37,9 @@ export class GamePage implements OnInit {
 
   }
   ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      if (window.screen.width <= 1200) this.mobile = true;
+    }
     this.fields = this.reader.readingFields();
     if (typeof document !== 'undefined') {
       const goldPrice = document.querySelector('#goldPrice') as HTMLElement;
@@ -55,14 +59,19 @@ export class GamePage implements OnInit {
       const onlinePrice = document.querySelector('#onlinePrice') as HTMLElement;
       const online1Income = document.querySelector('#online1Income') as HTMLElement;
       const online1Sell = document.querySelector('#online1Sell') as HTMLElement;
+      const online1Amount = document.querySelector('#online1Amount') as HTMLElement;
       const online2Income = document.querySelector('#online2Income') as HTMLElement;
       const online2Sell = document.querySelector('#online2Sell') as HTMLElement;
+      const online2Amount = document.querySelector('#online1Amount') as HTMLElement;
       const online3Income = document.querySelector('#online3Income') as HTMLElement;
       const online3Sell = document.querySelector('#online3Sell') as HTMLElement;
+      const online3Amount = document.querySelector('#online1Amount') as HTMLElement;
       const online4Income = document.querySelector('#online4Income') as HTMLElement;
       const online4Sell = document.querySelector('#online4Sell') as HTMLElement;
+      const online4Amount = document.querySelector('#online1Amount') as HTMLElement;
       const online5Income = document.querySelector('#online5Income') as HTMLElement;
       const online5Sell = document.querySelector('#online5Sell') as HTMLElement;
+      const online5Amount = document.querySelector('#online1Amount') as HTMLElement;
       const start = this.fields[this.startField];
       goldPrice.innerText = `$${(start.gold * 1000).toLocaleString('hu-HU')}`;
       goldAmount.innerText = '0 db';
@@ -81,14 +90,19 @@ export class GamePage implements OnInit {
       onlinePrice.innerText = `$${(start.onlineBuy * 1000).toLocaleString('hu-HU')}`;
       online1Income.innerText = `$${(start.onlineIncome1 * 1000).toLocaleString('hu-HU')}`;
       online1Sell.innerText = `$${(start.onlineSell1 * 1000).toLocaleString('hu-HU')}`;
+      online1Amount.innerText = '0 db';
       online2Income.innerText = `$${(start.onlineIncome2 * 1000).toLocaleString('hu-HU')}`;
       online2Sell.innerText = `$${(start.onlineSell2 * 1000).toLocaleString('hu-HU')}`;
+      online2Amount.innerText = '0 db';
       online3Income.innerText = `$${(start.onlineIncome3 * 1000).toLocaleString('hu-HU')}`;
       online3Sell.innerText = `$${(start.onlineSell3 * 1000).toLocaleString('hu-HU')}`;
+      online3Amount.innerText = '0 db';
       online4Income.innerText = `$${(start.onlineIncome4 * 1000).toLocaleString('hu-HU')}`;
       online4Sell.innerText = `$${(start.onlineSell4 * 1000).toLocaleString('hu-HU')}`;
+      online4Amount.innerText = '0 db';
       online5Income.innerText = `$${(start.onlineIncome5 * 1000).toLocaleString('hu-HU')}`;
       online5Sell.innerText = `$${(start.onlineSell5 * 1000).toLocaleString('hu-HU')}`;
+      online5Amount.innerText = '0 db';
     }
   }
   onBack(): void {
@@ -103,43 +117,99 @@ export class GamePage implements OnInit {
     console.log('Gold Info');
   }
 
+  goldBuy(): void {
+
+  }
+
+  goldSell(): void {
+
+  }
+
   mineInfo(): void {
     console.log('Mine Info');
+  }
+
+  mineBuy(): void {
+
+  }
+
+  mineSell(): void {
+
   }
 
   chocolateInfo(): void {
     console.log('Chocolate Info');
   }
 
+  chocolateBuy(): void {
+
+  }
+
+  chocolateSell(): void {
+
+  }
+
   flatInfo(): void {
     console.log('Flat Info');
+  }
+
+  flatBuy(): void {
+
   }
 
   pansionInfo(): void {
     console.log('Pansion Info');
   }
 
+  pansionBuy(): void {
+
+  }
+
   onlineInfo(): void {
     console.log('Online Info');
+  }
+
+  onlineBuy(): void {
+
   }
 
   level1Info(): void {
     console.log('Level 1 Info');
   }
 
+  online1Sell(): void {
+
+  }
+
   level2Info(): void {
     console.log('Level 2 Info');
+  }
+
+  online2Sell(): void {
+
   }
 
   level3Info(): void {
     console.log('Level 3 Info');
   }
 
+  online3Sell(): void {
+
+  }
+
   level4Info(): void {
     console.log('Level 4 Info');
   }
 
+  online4Sell(): void {
+
+  }
+
   level5Info(): void {
     console.log('Level 5 Info');
+  }
+
+  online5Sell(): void {
+
   }
 }
