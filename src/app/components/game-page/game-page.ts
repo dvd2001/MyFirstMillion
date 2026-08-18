43c3ae818ce1f883f200491859e8358f9759ba6a
Balogh Dávid <bedavid2001@gmail.com>
2026-08-18 10:05:00 +0200

    adding scroll to top functionality to roll function

--- src/app/components/game-page/game-page.ts
+++ src/app/components/game-page/game-page.ts
@@ -1071,6 +1071,7 @@ const body = document.querySelector('body') as HTMLElement;
           pansion.debtLevelUp();
         }
         this.update(true);
+if(typeof window !== 'undefined') window.scrollTo(0,0);
       }
     } catch (error) {
       alert('Nincs ilyen szám a dobókockán, próbáld újra!');
